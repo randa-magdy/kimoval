2e012e18f3e0e8a8d195a80c5bb0f86507c92d33
randa-magdy <randamagdy123@gmail.com>
2024-01-19 07:12:50 +0300

    fix: update files related to eslint to fix eslint issue in vs

---
+++ src/components/ui/Dropdown/Drooop.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Drooop() {
+  return (
+    <div>
+      Drooop
+    </div>
+  )
+}
---
+++ src/components/ui/Dropdown/testD.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function testD() {
+  return <div>testD</div>;
+}
