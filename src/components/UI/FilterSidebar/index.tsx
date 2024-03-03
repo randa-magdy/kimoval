63947041ed5dcec4167f27ce44b3aba82437e1f3
randa-magdy <randamagdy123@gmail.com>
2024-03-03 13:17:20 +0300

    fix: wrap filter sidebar by <Suspense> because using useSearchParams

--- src/components/UI/FilterSidebar/index.tsx
+++ src/components/UI/FilterSidebar/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, Suspense } from 'react';
 import Scrollbar from '../Scrollbar';
 import Space from '../Space';
 import BatteryFilters from './FilterTypes/BatteryFilters';
@@ -14,19 +14,21 @@ import SortByFilter from './FilterTypes/SortByFilter';
 const FilterSidebar: FC = () => {
   return (
     <aside className="my-5 sticky top-5 left-0">
-      <Scrollbar style={{ height: 'calc(100vh - 50px)' }} className="w-full">
-        <Space direction="vertical" className="pe-8">
-          <SortByFilter />
-          <PriceFilter />
-          <BrandsFilter />
-          <DesignFilters />
-          <HardwareFilters />
-          <CameraFilters />
-          <ConnectivityFilters />
-          <BatteryFilters />
-          <SoftwareFilters />
-        </Space>
-      </Scrollbar>
+      <Suspense>
+        <Scrollbar style={{ height: 'calc(100vh - 50px)' }} className="w-full">
+          <Space direction="vertical" className="pe-8">
+            <SortByFilter />
+            <PriceFilter />
+            <BrandsFilter />
+            <DesignFilters />
+            <HardwareFilters />
+            <CameraFilters />
+            <ConnectivityFilters />
+            <BatteryFilters />
+            <SoftwareFilters />
+          </Space>
+        </Scrollbar>
+      </Suspense>
     </aside>
   );
 };
